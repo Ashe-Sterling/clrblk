90a6078a140f8bffdf3305d23dbd6de7c92fa2b1
Ashe Sterling <ashe.sterling14@gmail.com>
2025-04-14 12:12:38 -0700

    Fixed crash caused by not validating ranged inputs as valid u8's; added debug output for invalid ranges.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::str;
+use std::io::{self, Write};
 extern crate clap;
 use clap::Parser;
 
+
 #[derive(Parser, Debug)]
 #[command(version, about = "A simple utility to show and test pretty colors")]
+
 struct Args {
     /// Width of blocks
     #[arg(short, long, default_value_t = 6)]
@@ -18,56 +21,68 @@ struct Args {
     values: Vec<String>,
 }
 
-fn print_block_ansi(color: u8, i: u8) {
-    // \x1b is the actual escape character (decimal 27)
-    print!("\x1b[48;5;{}m", color); 
-    for _ in 0..i {
-        print!(" ")
-    }
-    // Reset the color, then print a newline
-    print!("\x1b[0m\n");
+fn print_block_ansi(color: u8, width: u8) {
+    let mut buffer = String::new();
+    buffer.push_str(&format!("\x1b[48;5;{}m", color));
+    buffer.push_str(&" ".repeat(width.into()));
+    buffer.push_str("\x1b[0m\n");
+
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    let _ = handle.write_all(buffer.as_bytes());
+    let _ = handle.flush();
 }
 
 fn print_block_hex(hex_pairs: Vec<&str>, width: u8) {
     if hex_pairs.len() != 3 {
-        eprintln!("Hex input should be 6 characters, e.g. 'ffbbee'.");
+        eprintln!("Hex input should be 6 characters split into 3 parts, e.g. ['ff', 'bb', 'ee']");
         return;
     }
 
-    // Convert each pair from hex to u8
     let r = u8::from_str_radix(hex_pairs[0], 16).unwrap_or(0);
     let g = u8::from_str_radix(hex_pairs[1], 16).unwrap_or(0);
     let b = u8::from_str_radix(hex_pairs[2], 16).unwrap_or(0);
 
-    print!("\x1b[48;2;{};{};{}m", r, g, b);
+    let mut buffer = String::new();
+    buffer.push_str(&format!("\x1b[48;2;{};{};{}m", r, g, b));
+    buffer.push_str(&" ".repeat(width.into()));
+    buffer.push_str("\x1b[0m\n");
 
-    for _ in 0..width {
-        print!(" ");
-    }
-
-    print!("\x1b[0m\n");
+    let stdout = std::io::stdout();
+    let mut handle = stdout.lock();
+    let _ = handle.write_all(buffer.as_bytes());
+    let _ = handle.flush();
 }
 
-fn print_blocks_ansi(color1: u8, color2: u8, i: u8, inline: bool) {
+fn print_blocks_ansi(color1: u8, color2: u8, width: u8, inline: bool) {
     if inline {
+        let mut buffer = String::new();
         for color in color1..=color2 {
-            print!("\x1b[48;5;{}m", color); 
-            for _ in 0..i {
-                print!(" ")
-            }
+            buffer.push_str(&format!("\x1b[48;5;{}m", color));
+            buffer.push_str(&" ".repeat(width.into()));
         }
-        print!("\x1b[0m\n");
+        buffer.push_str("\x1b[0m\n");
+    
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+        let _ = handle.write_all(buffer.as_bytes());
+        let _ = handle.flush();    
+
     } else {
+        let mut buffer = String::new();
         for color in color1..=color2 {
-            print!("\x1b[48;5;{}m", color); 
-            for _ in 0..i {
-                print!(" ")
-            }
-            print!("\n");
+            buffer.push_str(&format!("\x1b[48;5;{}m", color));
+            buffer.push_str(&" ".repeat(width.into()));
+            buffer.push_str("\x1b[0m\n");
         }
-        print!("\x1b[0m\n");
+    
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+        let _ = handle.write_all(buffer.as_bytes());
+        let _ = handle.flush();
     }
 }
+
 fn named_color_to_ansi(input: &str) -> Option<u8> {
     match input.to_lowercase().as_str() {
         "black" => Some(0),
@@ -90,15 +105,12 @@ fn named_color_to_ansi(input: &str) -> Option<u8> {
     }
 }
 
-
 fn single(values: &[String], width: u8) {
     let color_input: &str = &values[0];
     if let Some(ansi_code) = named_color_to_ansi(color_input) {
         print_block_ansi(ansi_code, width);
-
     } else if let Ok(ansi_code) = color_input.parse::<u8>() {
         print_block_ansi(ansi_code, width);
-
     } else {
         let hexcode = color_input.strip_prefix('#').unwrap_or(color_input);
         let hex_pairs: Vec<&str> = hexcode
@@ -110,19 +122,29 @@ fn single(values: &[String], width: u8) {
     }
 }
 
-/// Called when we have more than two positional arguments
+/// Called when we have two positional arguments
 fn many(values: &[String], width: u8, inline: bool) {
-    let color_input1 = &values[0].parse::<u8>().unwrap();
-    let color_input2 = &values[1].parse::<u8>().unwrap();
-    print_blocks_ansi(*color_input1, *color_input2, width, inline);
+    if let Ok(color_input1) = values[0].parse::<u8>() {
+        if let Ok(color_input2) = values[1].parse::<u8>() {
+            print_blocks_ansi(color_input1, color_input2, width, inline);
+        } else {
+            println!("Error, input range end is not a valid ANSI color code (0-255, {} provided)", values[1]);
+        }
+    } else {
+        println!("Error, input range start is not a valid ANSI color code (0-255, {} provided)", values[0]);
+    }
 }
+
 
 fn main() {
     let args = Args::parse();
-
-    if args.values.len() >= 2 {
+    if args.values.len() == 2 {
         many(&args.values, args.width, args.inline);
-    } else {
+    } 
+    else if args.values.len() == 1 {
         single(&args.values, args.width);
     }
+    else {
+    }
 }
+ 
