7d261c408d89dcbcba1db6cdc924bdd3d535c30e
Ashe Sterling <ashe.sterling14@gmail.com>
2025-05-23 21:27:13 -0700

    Improved SIMD implementation

--- src/rainbow.rs
+++ src/rainbow.rs
@@ -5,7 +5,7 @@ use std::{
     time::Duration,
 };
 
-use rand::Rng;
+use rand::{rngs::ThreadRng, Rng};
 use termion::{
     async_stdin,
     clear,
@@ -19,9 +19,12 @@ use std::simd::{cmp::SimdPartialOrd, prelude::{Simd, SimdPartialEq}};
 //////////////////////////////////////////////////////////////////////////////////////////
 /// Random gradient looping per-cell, now with SIMD™
 
-pub fn crazyfn() -> std::io::Result<()> {
+// number of SIMD lanes for u8's
+const LANES: usize = 64;
+
+pub fn crazyfn() -> io::Result<()> {
     let mut stdout = stdout().into_raw_mode()?;
-    write!(stdout, "\x1b[?25l")?; // hide cursor
+    write!(stdout, "\x1b[?25l")?;
     stdout.flush()?;
 
     let mut stdin = async_stdin().bytes();
@@ -29,20 +32,19 @@ pub fn crazyfn() -> std::io::Result<()> {
 
     loop {
         if let Some(Ok(b)) = stdin.next() {
-            if b == 3 { // ctrl-c byte
+            if b == 3 {
                 break;
             }
         }
 
         buffer.resize();
         buffer.tick();
-        buffer.render(&mut stdout);
-        stdout.flush()?;
-
+        buffer.render(&mut stdout)?;
+        stdout.flush()?;  // ensure every frame is fully drawn
         thread::sleep(Duration::from_millis(20));
     }
 
-    write!(stdout, "\x1b[0m\x1b[?25h")?; // reset attrs + show cursor
+    write!(stdout, "\x1b[0m\x1b[?25h")?;
     stdout.flush()?;
     Ok(())
 }
@@ -56,13 +58,14 @@ struct Buffer {
     gr: Vec<u8>,
     gg: Vec<u8>,
     gb: Vec<u8>,
+    rng: ThreadRng,
 }
 
 impl Buffer {
     fn new() -> Self {
         let (w, h) = terminal_size().unwrap();
-        let mut rng = rand::rng();
         let size = (w as usize) * (h as usize);
+        let mut rng = rand::rng();
 
         let mut r = vec![0; size];
         let mut g = vec![0; size];
@@ -78,7 +81,7 @@ impl Buffer {
         rng.fill(&mut gg[..]);
         rng.fill(&mut gb[..]);
 
-        Buffer { width: w, height: h, r, g, b, gr, gg, gb }
+        Buffer { width: w, height: h, r, g, b, gr, gg, gb, rng }
     }
 
     fn resize(&mut self) {
@@ -86,106 +89,97 @@ impl Buffer {
         if w != self.width || h != self.height {
             self.width = w;
             self.height = h;
-            let mut rng = rand::rng();
             let size = (w as usize) * (h as usize);
 
-            self.r = vec![0; size];
-            self.g = vec![0; size];
-            self.b = vec![0; size];
-            self.gr = vec![0; size];
-            self.gg = vec![0; size];
-            self.gb = vec![0; size];
+            self.r.resize(size, 0);
+            self.g.resize(size, 0);
+            self.b.resize(size, 0);
+            self.gr.resize(size, 0);
+            self.gg.resize(size, 0);
+            self.gb.resize(size, 0);
 
-            rng.fill(&mut self.r[..]);
-            rng.fill(&mut self.g[..]);
-            rng.fill(&mut self.b[..]);
-            rng.fill(&mut self.gr[..]);
-            rng.fill(&mut self.gg[..]);
-            rng.fill(&mut self.gb[..]);
+            self.rng.fill(&mut self.r[..]);
+            self.rng.fill(&mut self.g[..]);
+            self.rng.fill(&mut self.b[..]);
+            self.rng.fill(&mut self.gr[..]);
+            self.rng.fill(&mut self.gg[..]);
+            self.rng.fill(&mut self.gb[..]);
         }
     }
 
     fn tick(&mut self) {
-        let mut rng = rand::rng();
-        const LANES: usize = 64;
         let len = self.r.len();
         let chunks = len / LANES;
 
         for i in 0..chunks {
             let base = i * LANES;
 
-            let r  = Simd::<u8, LANES>::from_slice(&self.r[base..base + LANES]);
-            let g  = Simd::<u8, LANES>::from_slice(&self.g[base..base + LANES]);
-            let b  = Simd::<u8, LANES>::from_slice(&self.b[base..base + LANES]);
-            let gr = Simd::<u8, LANES>::from_slice(&self.gr[base..base + LANES]);
-            let gg = Simd::<u8, LANES>::from_slice(&self.gg[base..base + LANES]);
-            let gb = Simd::<u8, LANES>::from_slice(&self.gb[base..base + LANES]);
+            let r_vec  = Simd::<u8, LANES>::from_slice(&self.r[base..][..LANES]);
+            let g_vec  = Simd::<u8, LANES>::from_slice(&self.g[base..][..LANES]);
+            let b_vec  = Simd::<u8, LANES>::from_slice(&self.b[base..][..LANES]);
+            let gr_vec = Simd::<u8, LANES>::from_slice(&self.gr[base..][..LANES]);
+            let gg_vec = Simd::<u8, LANES>::from_slice(&self.gg[base..][..LANES]);
+            let gb_vec = Simd::<u8, LANES>::from_slice(&self.gb[base..][..LANES]);
 
             let one = Simd::splat(1);
 
-            let r_new = r.simd_lt(gr)
-                .select(r + one, r.simd_gt(gr).select(r - one, r));
-            let g_new = g.simd_lt(gg)
-                .select(g + one, g.simd_gt(gg).select(g - one, g));
-            let b_new = b.simd_lt(gb)
-                .select(b + one, b.simd_gt(gb).select(b - one, b));
+            let r_new = r_vec.simd_lt(gr_vec)
+                .select(r_vec + one, r_vec.simd_gt(gr_vec).select(r_vec - one, r_vec));
+            let g_new = g_vec.simd_lt(gg_vec)
+                .select(g_vec + one, g_vec.simd_gt(gg_vec).select(g_vec - one, g_vec));
+            let b_new = b_vec.simd_lt(gb_vec)
+                .select(b_vec + one, b_vec.simd_gt(gb_vec).select(b_vec - one, b_vec));
 
-            r_new.copy_to_slice(&mut self.r[base..base + LANES]);
-            g_new.copy_to_slice(&mut self.g[base..base + LANES]);
-            b_new.copy_to_slice(&mut self.b[base..base + LANES]);
+            r_new.copy_to_slice(&mut self.r[base..][..LANES]);
+            g_new.copy_to_slice(&mut self.g[base..][..LANES]);
+            b_new.copy_to_slice(&mut self.b[base..][..LANES]);
 
-            let done = r_new.simd_eq(gr) & g_new.simd_eq(gg) & b_new.simd_eq(gb);
+            let done = r_new.simd_eq(gr_vec) & g_new.simd_eq(gg_vec) & b_new.simd_eq(gb_vec);
 
             let mut gr_buf = [0u8; LANES];
             let mut gg_buf = [0u8; LANES];
             let mut gb_buf = [0u8; LANES];
-            rng.fill(&mut gr_buf);
-            rng.fill(&mut gg_buf);
-            rng.fill(&mut gb_buf);
+            self.rng.fill(&mut gr_buf[..]);
+            self.rng.fill(&mut gg_buf[..]);
+            self.rng.fill(&mut gb_buf[..]);
 
             let new_gr = Simd::from_array(gr_buf);
             let new_gg = Simd::from_array(gg_buf);
             let new_gb = Simd::from_array(gb_buf);
 
-            new_gr.store_select(&mut self.gr[base..base + LANES], done);
-            new_gg.store_select(&mut self.gg[base..base + LANES], done);
-            new_gb.store_select(&mut self.gb[base..base + LANES], done);
+            new_gr.store_select(&mut self.gr[base..][..LANES], done);
+            new_gg.store_select(&mut self.gg[base..][..LANES], done);
+            new_gb.store_select(&mut self.gb[base..][..LANES], done);
         }
 
-        // scalar fallback for the len/64 remainder
-        let remainder = len % LANES;
-        let tail_start = chunks * LANES;
-        for i in 0..remainder {
-            let idx = tail_start + i;
-            for (v, g) in [(&mut self.r, &mut self.gr), (&mut self.g, &mut self.gg), (&mut self.b, &mut self.gb)] {
-                if v[idx] < g[idx] {
+        for idx in (chunks * LANES)..len {
+            for (v, t) in [(&mut self.r, &mut self.gr), (&mut self.g, &mut self.gg), (&mut self.b, &mut self.gb)] {
+                if v[idx] < t[idx] {
                     v[idx] += 1;
-                } else if v[idx] > g[idx] {
+                } else if v[idx] > t[idx] {
                     v[idx] -= 1;
                 } else {
-                    g[idx] = rng.random();
+                    t[idx] = self.rng.random();
                 }
             }
         }
     }
 
-    fn render(&self, out: &mut impl Write) {
-        write!(out, "{}{}", cursor::Goto(1, 1), clear::All).unwrap();
+    fn render(&self, out: &mut impl Write) -> io::Result<()> {
+        write!(out, "{}{}", cursor::Goto(1, 1), clear::All)?;
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = (row as usize) * (self.width as usize) + (col as usize);
-                write!(out, "\x1b[48;2;{};{};{}m ", self.r[idx], self.g[idx], self.b[idx]).unwrap();
+                write!(out, "\x1b[48;2;{};{};{}m ", self.r[idx], self.g[idx], self.b[idx])?;
             }
             if row < self.height - 1 {
-                write!(out, "\r\n").unwrap();
+                write!(out, "\r\n")?;
             }
         }
-        write!(out, "\x1b[0m").unwrap();
+        write!(out, "\x1b[0m")?;
+        Ok(())
     }
 }
-
-/// End random gradient looping per-cell
-//////////////////////////////////////////////////////////////////////////////////////////
 
 
 
